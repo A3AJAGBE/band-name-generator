29dfbeb08a72bb77755e8cc73eb7f36f6e3ff039
AJAGBE AMINAT ABOLORE <ajagbeaminat95@gmail.com>
2020-11-09 15:44:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+An application to generate band name for users using favorite city and number of people in the band
+"""
+
+print("Welcome to A3AJAGBE Band Name Generator\n")
+
+City = input("Where is your favorite city?\n")
+
+Num = input("Number of people in the band?\n")
+
+print("Band Name could be " + City + " " + Num)
