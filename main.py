fe27cd96e1d3cdcaa9b722dfc48e92c683a95385
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-18 07:28:28 +0000

    Add inputs

--- main.py
+++ main.py
@@ -7,4 +7,12 @@ from logo import logo
 
 # Default application displays
 print(logo)
-print("The following inputs will be used to generate a bandname\n")
+print("The following inputs will be used to generate a band name\n")
+
+# Inputs
+street = input("Name of your street?\n").title()
+color = input("What is your favourite color?\n").title()
+
+# Output
+bandname = street + " " + color
+print(f"Band Name generated is: {bandname}")
